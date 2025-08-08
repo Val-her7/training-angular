986307e9edfd130b1e81078c42c3cbfaee11e3c2
Valher7 <Valentin.herman.pro@gmail.com>
2025-08-08 15:09:49 +0200

    Practising intput and output

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { PlayingCard } from "./components/playing-card/playing-card.component";
 import { Monster } from './models/monster.model';
-import { SearchBar } from "./components/search-bar/search-bar";
+import { SearchBar } from "./components/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css',
   imports: [PlayingCard, SearchBar]
@@ -15,10 +16,19 @@ export class App {
 
   monster1!: Monster;
 
+  search = '';
+
+  count: number = 0;
+
   constructor() {
     this.monster1 = new Monster();
     this.monster1.name = "Pik";
     this.monster1.hp = 40;
     this.monster1.figureCaption = "N°002 Pik"
   }
+
+  increaseCount() {
+    this.count ++;
+  }
+
 }
