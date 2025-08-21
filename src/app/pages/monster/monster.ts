337bbac59e516a99c4a0232d5f7eeca8a8603fb9
Valher7 <Valentin.herman.pro@gmail.com>
2025-08-21 16:39:34 +0200

    Practising FormControl

--- src/app/pages/monster/monster.ts
+++ src/app/pages/monster/monster.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-monster',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './monster.html',
   styleUrl: './monster.css'
 })
@@ -16,6 +17,9 @@ export class MonsterComponent implements OnInit, OnDestroy {
   monsterId = signal<number | undefined>(undefined);
   routeSubscription: Subscription | null = null;
 
+  name = new FormControl('', [Validators.required]);
+  hp = new FormControl(0, [Validators.required, Validators.min(1), Validators.max(200)]);
+
   ngOnInit(): void {
       this.routeSubscription = this.route.params.subscribe(params => {
         this.monsterId.set(params['id'] ? parseInt(params['id']) : undefined);
@@ -26,9 +30,9 @@ export class MonsterComponent implements OnInit, OnDestroy {
       this.routeSubscription?.unsubscribe();
   }
 
-  next() {
-    let nextId = this.monsterId() || 0;
-    nextId++;
-    this.router.navigate(['/monster/' + nextId]);
+  submit(event: Event) {
+    event.preventDefault;
+    console.log(this.name.value);
+    
   }
 }
